39790ba2c914c2af39d2ab975b2680c83d48ccb1
xuantien0201 <xtien1812@gmail.com>
2025-11-05 21:10:26 +0700

    new 8

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? 'https://mypickle-production.up.railway.app' : 'http://localhost:3000'),
 });
 
 export default instance;
